764768d7b02a2806ad9f74b588ee16216b67b505
FanFusheng <ffs@ppno.net>
2025-04-05 10:21:01 +0800

    refactor(internal/service): 重构 greeter 服务并添加文档注释
    - 为 greeterService 结构体和其成员添加了详细的文档注释
    - 为 NewGreeterService 和 SayHello 方法添加了详细的文档注释
    - 优化了代码格式和顺序，提高了代码可读性

--- internal/service/greeter.go
+++ internal/service/greeter.go
@@ -15,13 +15,26 @@ import (
 )
 
 type (
+	// greeterService 实现了 GreeterHTTPServer 接口，提供问候服务。
 	greeterService struct {
+		// logger 用于服务日志记录。
 		logger kit_log.Logger
-		conf   *app_conf.Config
-		uc     app_biz.GreeterUsecase
+		// conf 存储服务配置信息。
+		conf *app_conf.Config
+		// uc 用于处理问候相关的业务逻辑。
+		uc app_biz.GreeterUsecase
 	}
 )
 
+// NewGreeterService 创建一个新的 GreeterHTTPServer 服务实例。
+//
+// 参数：
+//   - logger：日志记录器，用于服务日志记录。
+//   - conf：服务配置信息。
+//   - uc：问候用例的业务逻辑实现。
+//
+// 返回：
+//   - app_helloworld_v1.GreeterHTTPServer：问候服务的实现实例。
 func NewGreeterService(logger kit_log.Logger, conf *app_conf.Config, uc app_biz.GreeterUsecase) app_helloworld_v1.GreeterHTTPServer {
 	return &greeterService{
 		logger: logger,
@@ -30,6 +43,15 @@ func NewGreeterService(logger kit_log.Logger, conf *app_conf.Config, uc app_biz.
 	}
 }
 
+// SayHello 发送问候消息。
+//
+// 参数：
+//   - ctx：上下文信息。
+//   - in：包含问候请求的参数。
+//
+// 返回：
+//   - *app_helloworld_v1.HelloReply：问候响应。
+//   - error：可能发生的错误。
 func (s *greeterService) SayHello(ctx context.Context, in *app_helloworld_v1.HelloRequest) (*app_helloworld_v1.HelloReply, error) {
 	return nil, nil
 }
