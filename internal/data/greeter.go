946d966b198fbb3b011b30ea51641f1ba98ee93d
FanFusheng <ffs@ppno.net>
2025-04-05 10:20:45 +0800

    feat(internal): 添加数据层实现
    - 新增 data 包，实现数据层公共依赖和基础操作
    - 添加 greeter.go 文件，实现 Greeter 相关的数据访问操作
    - 使用 Wire 进行依赖注入，提高代码可维护性

---
+++ internal/data/greeter.go
@@ -0,0 +1,105 @@
+// Copyright 2025 fsyyft-go
+//
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+package data
+
+import (
+	"context"
+
+	kit_log "github.com/fsyyft-go/kit/log"
+
+	app_biz "github.com/fsyyft-go/kratos-layout/internal/biz"
+	app_conf "github.com/fsyyft-go/kratos-layout/internal/conf"
+)
+
+// greeterRepo 实现了 app_biz.GreeterRepo 接口，提供 Greeter 相关的数据访问操作。
+type greeterRepo struct {
+	// log 用于记录日志信息。
+	log kit_log.Logger
+	// conf 存储应用配置信息。
+	conf *app_conf.Config
+	// data 提供数据层公共依赖。
+	data Data
+}
+
+// NewGreeterRepo 创建一个新的 GreeterRepo 实例。
+//
+// 参数：
+//   - logger：日志记录器，用于记录操作日志。
+//   - conf：应用配置信息。
+//   - data：数据层公共依赖。
+//
+// 返回：
+//   - GreeterRepo 接口实现。
+func NewGreeterRepo(logger kit_log.Logger, conf *app_conf.Config, data Data) app_biz.GreeterRepo {
+	return &greeterRepo{
+		log:  logger,
+		conf: conf,
+		data: data,
+	}
+}
+
+// Save 保存一个 Greeter 实体，返回保存后的实体和可能的错误。
+//
+// 参数：
+//   - ctx：上下文信息。
+//   - g：需要保存的 Greeter 实体。
+//
+// 返回：
+//   - 保存后的 Greeter 实体。
+//   - 可能发生的错误。
+func (r *greeterRepo) Save(ctx context.Context, g *app_biz.Greeter) (*app_biz.Greeter, error) {
+	return g, nil
+}
+
+// Update 更新一个 Greeter 实体，返回更新后的实体和可能的错误。
+//
+// 参数：
+//   - ctx：上下文信息。
+//   - g：需要更新的 Greeter 实体。
+//
+// 返回：
+//   - 更新后的 Greeter 实体。
+//   - 可能发生的错误。
+func (r *greeterRepo) Update(ctx context.Context, g *app_biz.Greeter) (*app_biz.Greeter, error) {
+	return g, nil
+}
+
+// FindByID 根据 ID 查找 Greeter 实体，返回查找到的实体和可能的错误。
+//
+// 参数：
+//   - ctx：上下文信息。
+//   - id：Greeter 实体的唯一标识。
+//
+// 返回：
+//   - 查找到的 Greeter 实体。
+//   - 可能发生的错误。
+func (r *greeterRepo) FindByID(context.Context, int64) (*app_biz.Greeter, error) {
+	return nil, nil
+}
+
+// ListByHello 根据 hello 字段查找 Greeter 实体列表，返回查找到的实体列表和可能的错误。
+//
+// 参数：
+//   - ctx：上下文信息。
+//   - hello：查询条件。
+//
+// 返回：
+//   - Greeter 实体列表。
+//   - 可能发生的错误。
+func (r *greeterRepo) ListByHello(context.Context, string) ([]*app_biz.Greeter, error) {
+	return nil, nil
+}
+
+// ListAll 获取所有 Greeter 实体列表，返回实体列表和可能的错误。
+//
+// 参数：
+//   - ctx：上下文信息。
+//
+// 返回：
+//   - 所有 Greeter 实体列表。
+//   - 可能发生的错误。
+func (r *greeterRepo) ListAll(context.Context) ([]*app_biz.Greeter, error) {
+	return nil, nil
+}
