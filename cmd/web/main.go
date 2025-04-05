7b64cdf07b64084bc7a7710ee68af18c1b7c987c
FanFusheng <ffs@ppno.net>
2025-04-05 11:09:38 +0800

    feat: 添加 web 服务入口程序
    - 新增 main.go 文件，实现 web 服务的入口程序
    - 初始化配置和依赖注入
    - 启动 web 服务

---
+++ cmd/web/main.go
@@ -0,0 +1,17 @@
+// Copyright 2025 fsyyft-go
+//
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+// Package main 实现了任务执行器的入口程序。
+// 该程序负责初始化配置、依赖注入，并启动任务执行器。
+package main
+
+import (
+	app_web "github.com/fsyyft-go/kratos-layout/internal/app/web"
+)
+
+func main() {
+	// 应用程序入口。
+	// 测试过在某些情况下，使用 wire 生成代码时，会报错，可能是因为这时 main 包的原因，所以这里只包含入口。
+	app_web.Run()
+}
